80271dc45b1dd427e0d70e1a16a449b3227196d5
Matthew Chan <mchan@genetec.com>
2020-01-31 20:55:28 -0500

    Add missing folders to master

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Matthew Chan on 2020-01-31.
+//  Copyright © 2020 Matthew Chan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
